f08a3173513909383a9c6c744ab1cb2c8aafb18b
Light510 <attaclp@gmail.com>
2025-02-24 17:31:58 +0700

    Major Fix, fiturku ga work akhirnya ku kembaliin seperti semula :d

--- script.js
+++ script.js
@@ -74,14 +74,8 @@ captureButton.addEventListener('click', () => {
         }
 
         if (photoRoll.length === 3) {
-            const scaleFactor = 0.87; // Mengecilkan foto menjadi 87% dari ukuran asli
-            const spacing = 8; // Jarak antar foto
-            const scaledWidth = canvas.width * scaleFactor;
-            const scaledHeight = canvas.height * scaleFactor;
-
-            rollCanvas.width = scaledWidth;
-            rollCanvas.height = scaledHeight * 3 + spacing * 2; // 3 foto + 2x jarak antar foto
-
+            rollCanvas.width = canvas.width;
+            rollCanvas.height = canvas.height * 3;
             rollContext.fillStyle = rollColorPicker.value;
             rollContext.fillRect(0, 0, rollCanvas.width, rollCanvas.height);
 
@@ -89,8 +83,7 @@ captureButton.addEventListener('click', () => {
                 let img = new Image();
                 img.src = photo;
                 img.onload = () => {
-                    let yPos = index * (scaledHeight + spacing);
-                    rollContext.drawImage(img, 0, yPos, scaledWidth, scaledHeight);
+                    rollContext.drawImage(img, 0, index * canvas.height);
                 };
             });
 
